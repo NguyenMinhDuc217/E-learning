d82d6e3af35a20e42df226313f48b9b5f2a9baf1
unknown <hoang.phan804@gmail.com>
2021-12-22 19:29:36 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dang-nhap', [LoginController::class, 'login'])->name("dang-nhap")->middleware('guest');
